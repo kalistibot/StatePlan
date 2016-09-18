6ec399b3404d81a00ee018c8074a37fb6545de2c
= <kalistibot@yahoo.com>
2016-09-18 15:14:10 -0500

    adding backup mechanism for database file

---
+++ databack.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import shutil
+import datetime
+
+file = "/MedicAid.sqlite3"
+today = str(datetime.date.today())
+lastmonth = str(datetime.date.today() - datetime.timedelta(days=30)
+)
+dbdir = os.getcwd()
+dbsrc = dbdir + file
+dbdest = dbdir + "/databack/"+ file + today
+
+shutil.copy2(dbsrc,dbdest)
+try:
+    os.remove(dbdir+"databack/"+ file + lastmonth)
+except:
+    print"nothing removed"
